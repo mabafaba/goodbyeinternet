3be2e603bf0760e2d6e8d8fce29d26f825f5c0cf
m <barner.mar@gmail.com>
2016-05-05 10:43:08 +0100

    new severity parameter new mutation rate parameter new comments

--- goodbyeinternet.js
+++ goodbyeinternet.js
@@ -1,27 +1,37 @@
 
-function selfdestruct(){
-String.prototype.replaceAt=function(index, character) {
-    return this.substr(0, index) + character + this.substr(index+character.length);
-}
+// selfdestruct():
+	// switches two characters in the html code
+	// then waits for a random amount of time
+	// then the function calls itself
+
+// parameters:
+	// mutationrate:
+		// the maximum time in seconds to wait to repeat mutation
+	// severity: integer 1,2 or 3 for 3 levels of severity
+ 		// 0: only affects the content of element with id 'goodbyeinternet'
+ 		// 1: affects everything in <body></body>
+ 		// 2: affects entire html top to bottom. Should obviously be the default.
 
 
+// list of object from which one is destroyed based on severity level.
+what_to_destruct = ['#goodbyeinternet','body','html'];
 
+function selfdestruct(severity = 1,mutationrate= 5){
+console.log(what_to_destruct[severity]);
+html_source = $(what_to_destruct[severity]).html();
 
-
-test = $('#goodbyeinternet').html();
-
-// test = "1 2 3 4 5 6 7 8 9 0"
-for(var i=0;i<=1;i++)
+// loop not in use; could switch more than one pair of characters per mutation
+for(var i=0;i<=0;i++)
 {
-	a = parseInt(Math.random()*test.length);
-	b = parseInt(Math.random()*test.length);
-if(test[a] != '<' & test[a] != '>' & test[b] != '<' & test[b] != '>' ){
-aletter = test[a];
-test = test.replaceAt(a, test[b]);
-test = test.replaceAt(b,aletter);
-	console.log(test);
+	a = parseInt(Math.random()*html_source.length);
+	b = parseInt(Math.random()*html_source.length);
+if(html_source[a] != '<' & html_source[a] != '>' & html_source[b] != '<' & html_source[b] != '>' ){
+aletter = html_source[a];
+html_source = html_source.replaceAt(a, html_source[b]);
+html_source = html_source.replaceAt(b,aletter);
+// console.log(html_source);
 
-$('#goodbyeinternet').html(test);
+$(what_to_destruct[severity]).html(html_source);
 }
 }
 
@@ -41,3 +51,11 @@ setTimeout(
 		Math.random()*5000);
 
 });
+
+
+// custom function to replace characters in strings:
+// credit to StackOverflow user Cem Kalyoncu
+// http://stackoverflow.com/questions/1431094/how-do-i-replace-a-character-at-a-particular-index-in-javascript 
+String.prototype.replaceAt=function(index, character) {
+    return this.substr(0, index) + character + this.substr(index+character.length);
+}
